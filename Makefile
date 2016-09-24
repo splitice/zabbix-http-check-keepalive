de57f5cd7fe380b2bfda2bf19f494543a114bea4
splitice <mat999@gmail.com>
2016-09-24 11:33:31 +1000

    define IPv4 tests

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 zabbix_http_check_keepalive: zabbix_http_check_keepalive.cpp hck_engine.cpp
 	g++ -fPIC -shared -o zabbix_http_check_keepalive.so zabbix_http_check_keepalive.cpp hck_engine.cpp -I../../../include
+
+tests: test.cpp hck_engine.cpp
+	g++ -fPIC -shared -o zabbix_http_check_keepalive.so test.cpp hck_engine.cpp -I../../../include
---
+++ test.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define CATCH_CONFIG_MAIN
+#include "Catch.hpp"
+#include "hck_engine.h"
+
+static bool shutdown = true;
+extern bool running;
+
+void hck_log(int level, const char *fmt, ...){
+	va_list args;
+    va_start(args, fmt);
+    vprintf(errbuf, fmt, args);
+    va_end(args);
+	puts("");
+}
+
+static void start_engine(){
+	if (fork() == 0){
+		zbx_setproctitle("zabbix_proxy: http check keepalive #1");
+		shutdown = false;
+		processing_thread();
+		shutdown = true;
+		exit(1);
+	}
+}
+
+TEST_CASE( "IPv4 Test to Online Host" ) {
+    start_engine();
+	
+	unsigned short result = execute_check("216.58.194.174","80");
+	
+	REQUIRE( result == 1 );
+	
+	running = false;
+	while(!shutdown){
+		sleep(1);
+	}
+}
+
+TEST_CASE( "IPv4 Test to Offline Host" ) {
+    start_engine();
+	
+	unsigned short result = execute_check("127.123.123.123","14");
+	
+	REQUIRE( result == 0 );
+	
+	running = false;
+	while(!shutdown){
+		sleep(1);
+	}
+}
